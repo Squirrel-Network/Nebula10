b3cb086f5abb64e2fd11e7dd00b36266c0328bdc
ByteCntrl <bytecntrl@gmail.com>
2023-05-03 17:14:13 +0200

    Update languages

--- languages/languages.py
+++ languages/languages.py
@@ -33,12 +33,15 @@ def get_group_lang(update: Update) -> str | None:
 
 
 def get_lang(update: Update) -> Lang:
-    lang = get_group_lang(update) or Session.config.DEFAULT_LANGUAGE
+    lang = Session.config.DEFAULT_LANGUAGE
 
     if (
         update.effective_chat.type == "private" and 
         update.effective_user.language_code in ("it", "en")
     ):
         lang = update.effective_user.language_code
+        
+    else:
+        lang = get_group_lang(update) or lang
     
     return Session.lang[lang.lower()]
