3ca7cfa6beed2906c7d041fd35aace01cbcee6f5
BluLupo <hersel1991@gmail.com>
2023-05-05 00:02:59 +0200

    fix welcome

--- core/handlers/chat_handlers/welcome.py
+++ core/handlers/chat_handlers/welcome.py
@@ -2,22 +2,18 @@
 # -*- coding: utf-8 -*-
 
 # Copyright SquirrelNetwork
-
-from telegram import Update
-from telegram.ext import ContextTypes
 from core.utilities.entities import TelegramObjects
 from core.utilities.functions import save_group
 from core.database.repository.group import GroupRepository
 
-
-async def new_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def new_member(update, context):
     bot = context.bot
-    chat = TelegramObjects(update,context).chat_object()
-
-    row = GroupRepository().getById(chat.id)
-    if row:
-        print("UPDATE")
-    else:
-        save_group(update.effective_chat.id, update.effective_chat.title)
-
-    await bot.send_message(chat, "Welcome")
+    chat = TelegramObjects(update, context).chat_object()
+    for member in update.message.new_chat_members:
+        row = GroupRepository().getById(chat.id)
+        print(member)
+        if row:
+            print("UPDATE GROUP")
+        else:
+            save_group(update.effective_chat.id, update.effective_chat.title)
+        await bot.send_message(chat.id, "Welcome {} in {}".format(member.first_name, update.message.chat.title))
