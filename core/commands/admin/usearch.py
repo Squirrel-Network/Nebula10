764ab345e6a25e65da81a291e8f4f7480ce60125
BluLupo <hersel1991@gmail.com>
2023-06-07 21:26:19 +0200

    add lang in /usearch

--- core/commands/admin/usearch.py
+++ core/commands/admin/usearch.py
@@ -14,15 +14,15 @@ from core.decorators import check_role, delete_command
 from core.utilities.enums import Role
 from core.utilities.message import message
 from telegram.ext import ContextTypes
+from languages import get_lang
 
 
 @check_role(Role.OWNER, Role.CREATOR, Role.ADMINISTRATOR)
 @delete_command
 async def init(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    lang = get_lang(update)
 
-    await message(
-        update, context, "Open Search in Private\nNeed Start the bot in private"
-    )
+    await message(update, context, lang["USEARCH_MSG"])
 
     await context.bot.send_message(
         update.effective_user.id,
