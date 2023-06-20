9b6b46d0f4cf2e901a84f35a4808defd3a3c326d
@BluLupo <hersel1991@gmail.com>
2023-06-20 10:21:20 +0200

    delete Dashboard repository

--- core/database/repository/__init__.py
+++ core/database/repository/__init__.py
@@ -5,7 +5,6 @@
 
 __all__ = [
     "CommunityRepository",
-    "DashboardRepository",
     "GroupRepository",
     "GroupLanguageRepository",
     "SuperbanRepository",
@@ -13,7 +12,6 @@ __all__ = [
 ]
 
 from .community import CommunityRepository
-from .dashboard import DashboardRepository
 from .group import GroupRepository
 from .group_language import GroupLanguageRepository
 from .superban import SuperbanRepository
