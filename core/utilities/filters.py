097ccc7c00786852e574cbb0a7f035247690ef5b
ByteCntrl <bytecntrl@gmail.com>
2023-07-20 20:24:39 +0200

    Update filters

--- core/utilities/filters.py
+++ core/utilities/filters.py
@@ -3,8 +3,57 @@
 
 # Copyright SquirrelNetwork
 
-from telegram import Message
+from telegram import Message, Update
+from telegram.ext import ContextTypes
 from telegram.ext.filters import MessageFilter
+
+
+class Filter:
+    async def __call__(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        raise NotImplementedError
+
+    def __invert__(self):
+        return InvertFilter(self)
+
+    def __and__(self, other):
+        return AndFilter(self, other)
+
+    def __or__(self, other):
+        return OrFilter(self, other)
+
+
+class InvertFilter(Filter):
+    def __init__(self, base):
+        self.base = base
+
+    async def __call__(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        x = await self.base(update, context)
+
+        return not x
+
+
+class AndFilter(Filter):
+    def __init__(self, base, other):
+        self.base = base
+        self.other = other
+
+    async def __call__(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        x = await self.base(update, context)
+        y = await self.other(update, context)
+
+        return x and y
+
+
+class OrFilter(Filter):
+    def __init__(self, base, other):
+        self.base = base
+        self.other = other
+
+    async def __call__(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        x = await self.base(update, context)
+        y = await self.other(update, context)
+
+        return x or y
 
 
 class _ReplyText(MessageFilter):
