f221824fb44d43b4e551f6fd774604899d3cfb0c
ByteCntrl <bytecntrl@gmail.com>
2023-08-08 19:15:06 +0200

    Update database

--- core/database/models/groups.py
+++ core/database/models/groups.py
@@ -50,6 +50,7 @@ class Groups(Model):
     antiflood_seconds = fields.IntField(default=1)
     antistorm_max_users = fields.IntField(default=15)
     antistorm_seconds = fields.IntField(default=3)
+    set_captcha = fields.BooleanField(default=False)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
