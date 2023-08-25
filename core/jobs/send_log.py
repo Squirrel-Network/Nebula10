e5d812a3e9598cc2e50c5bfa0902a1511af2be17
@BluLupo <hersel1991@gmail.com>
2023-08-25 17:31:16 +0200

    update jobs logic and prepare jobs log

---
+++ core/jobs/send_log.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright SquirrelNetwork
+
+from telegram.ext import ContextTypes
+
+#This function sends the log file to the Telegram log channel
+#async def send_log(context: ContextTypes.DEFAULT_TYPE):
+async def send_log():
+    #bot = context.bot
+    #await bot.send_document(chat_id='-1001540824311',document=open('file.log', 'rb'))
+    print("TEST LOG")
